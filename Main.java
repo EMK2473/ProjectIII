1bf0fbcc42c546b4a87c72a412f34935c5aea67e
Eric Keeton <142048130+EMK2473@users.noreply.github.com>
2024-09-29 19:24:39 -0400

    added 5 new students; for each looped through studentArray; auto calling toString/print method

--- Main.java
+++ Main.java
@@ -1,14 +1,25 @@
 import java.util.ArrayList;
 
+
 public class Main {
 
     public static void main(String[] args) {
         // create array of student objects
         ArrayList<Student> studentsArray = new ArrayList<>();
         // add students into array
-        studentsArray.add(new Student("eric", "keeton", "senior", "male", "english"));
+        studentsArray.add(new Student("Tony", "Stark", "Senior", "Male", "Engineering"));
+        studentsArray.add(new Student("Natasha", "Romanoff", "Junior", "Female", "Martial Arts"));
+        studentsArray.add(new Student("Steve", "Rogers", "Senior", "Male", "History"));
+        studentsArray.add(new Student("Wanda", "Maximoff", "Junior", "Female", "Psychology"));
+        studentsArray.add(new Student("Thanos", "Titan", "Graduate", "Male", "Philosophy"));
+        
         // print student info
+        // for each student object in studentsArray, print each student object (calls toString method)
         System.out.println(studentsArray.get(0));
+
+        for(Student student : studentsArray){
+            System.out.println(student);
+        }
 
     }
 
